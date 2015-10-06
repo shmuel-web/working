a6b7de7059eb67a653349434b66e58ed0299a66a
shmuel <disraelishmuel@gmail.com>
2015-10-06 14:19:40 +0300

    simple page load fade animation

--- js/main-en.js
+++ js/main-en.js
@@ -30,6 +30,23 @@ $(document).ready(function() {
 				   	$('.navicon').removeClass('fa-times').addClass('fa-bars');
 				}
 			});
+			// preloader
+			//==============================
+			// makes sure the whole site is loaded
+				
+				jQuery(window).load(function() {
+				
+				        // will first fade out the loading animation
+				
+				    jQuery("#status").fadeOut();
+				
+				        // will fade out the whole DIV that covers the website.
+				
+				    jQuery("#preloader").delay(1000).fadeOut("slow");
+				
+				})
+			//end
+			//=============================
 				
 
 		
