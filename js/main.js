006b5a27bf5d07d0020352f682deb62fcc7005b6
shmuel <disraelishmuel@gmail.com>
2015-10-06 13:41:52 +0300

    canceld page loader

--- js/main.js
+++ js/main.js
@@ -30,54 +30,5 @@ $(document).ready(function() {
 				   	$('.navicon').removeClass('fa-times').addClass('fa-bars');
 				}
 			});
-
-			// page loader
-			// =====================================================================
-			function changePage(url, bool) {
-			    isAnimating = true;
-			    // trigger page animation
-			    $('body').addClass('page-is-changing');
-			    //...
-			    loadNewContent(url, bool);
-			    //...
-			}
-
-			function loadNewContent(url, bool) {
-			  	var newSectionName = 'cd-'+url.replace('.html', ''),
-			  		section = $('<div class="cd-main-content '+newSectionName+'"></div>');
-			  		
-			  	section.load(url+' .cd-main-content > *', function(event){
-			    	// load new content and replace <main> content with the new one
-			      	$('main').html(section);
-			      	//...
-			      	$('body').removeClass('page-is-changing');
-			      	//...
-			 
-			      	if(url != window.location){
-			        	//add the new page to the window.history
-			        	window.history.pushState({path: url},'',url);
-			      	}
-				});
-			}
-
-			$('main').on('click', '[data-type="page-transition"]', function(event){
-			    event.preventDefault();
-			    //detect which page has been selected
-			    var newPage = $(this).attr('href');
-			    //if the page is not animating - trigger animation
-			    if( !isAnimating ) changePage(newPage, true);
-			});
-
-			$(window).on('popstate', function() {
-			    var newPageArray = location.pathname.split('/'),
-			        //this is the url of the page to be loaded 
-			        newPage = newPageArray[newPageArray.length - 1];
-			    if( !isAnimating ) changePage(newPage);
-			});
-			
-			//page loader end
-			// ========================================================= 
-
-		
 });
 
